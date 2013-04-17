5b72ff31def7c61c16facbd90dd9c224ad9086ab
mahata <nori@mahata.net>
2013-04-17 00:34:46 -0700

    writing presentation material for Ruby Meetup.

--- web.py
+++ web.py
@@ -8,14 +8,25 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app = Flask(__name__)
 app.config.update(DEBUG=bool(os.getenv('DEBUG')), SECRET_KEY=os.getenv('SECRET_KEY'))
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/sample/<app_name>')
 def sample(app_name):
     return render_template('sample/' + app_name + '.html')
 
+
+@app.route('/presentation/')
+def presentation_index():
+    return render_template('presentation.html')
+
+
+@app.route('/presentation/<date>')
+def presentation(date):
+    return render_template('presentation/' + date + '.html')
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
